1511be15216d64dc52c5feeecda0859061dadced
phillaung <yellowpete.kog@gmail.com>
2023-11-29 18:13:25 -0800

    lab part one home, about, and finches pages set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Coco', 'breed': 'yellowtail', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sunny', 'breed': 'parrot', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
