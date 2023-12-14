ac8f0850c67e98091599e2ad174646a559bd04da
phillaung <yellowpete.kog@gmail.com>
2023-12-13 18:55:04 -0800

    toys model created and adding toys to finches

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
-    
+
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -19,10 +19,13 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
     'finch': finch,
-    'feeding_form': feeding_form
+    'feeding_form': feeding_form,
+    'toys': toys_finch_doesnt_have
   })
 
 def add_feeding(request, finch_id):
@@ -33,9 +36,13 @@ def add_feeding(request, finch_id):
         new_feeding.save()
     return redirect('detail', finch_id=finch_id)
 
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'description', 'age']
   success_url = '/finches'
 
 class FinchUpdate(UpdateView):
