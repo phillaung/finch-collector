a3e2bdb506141d98407b69a855452b1f4e7250ae
phillaung <yellowpete.kog@gmail.com>
2023-12-13 22:50:07 -0800

    user log in

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Toy(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
-    
+
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
